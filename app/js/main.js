6b1fc29c47688f81505c04008ea2b3e2ce0ff34b
romahomework <64336557+romahomework@users.noreply.github.com>
2020-07-15 14:56:02 +0300

    new

---
+++ app/js/main.js
@@ -0,0 +1,11 @@
+$(function(){
+
+    $('.products__slider').slick({
+        prevArrow:   '<button class="slider-btn slider-btn__left"><svg width="60" height="48" viewBox="0 0 60 48" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M34.2184 16L26 24L34.2184 32" /></svg></button>',
+        nextArrow: '<button class="slider-btn slider-btn__right"><svg width="60" height="48" viewBox="0 0 60 48" fill="none" xmlns="http://www.w3.org/2000/svg"> <path d="M25.7816 32L34 24L25.7816 16"/></svg></button>',
+        infinite : false,
+
+    });
+
+
+});
